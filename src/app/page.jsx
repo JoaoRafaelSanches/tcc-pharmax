5aeb36320116ddd969aa288ccb2a5d500e402e4f
João Rafael Sanches <jsanchesedantas@gmail.com>
2025-08-05 22:49:51 -0300

    1 aula next

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div>
+    <h1> hello world </h1>
+    <p>primeiro exemplo no next</p>
+    <div className="quadrado"></div>
+    </div>
+  )
+}
+
+
