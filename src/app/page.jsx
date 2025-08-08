6b16da5377baed9b5233460e7382be374c41bdbe
João Rafael Sanches <jsanchesedantas@gmail.com>
2025-08-07 22:47:44 -0300

    aula 2 next.js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
-
+import style from './page.module.css'
 export default function Home() {
   return (
-    <div>
-    <h1> hello world </h1>
-    <p>primeiro exemplo no next</p>
+    <div className="container">
+    <h1 className={style.tituloHome}> Titulo 1</h1>
+    <h2> Titulo 2</h2>
+    <h3> Titulo 3</h3>
+    <p className="txtDestaque">primeiro exemplo no next </p>
+    <p>primeiro texto next</p>
+    <small>texto next</small>
+    <br />
     <div className="quadrado"></div>
     </div>
   )
